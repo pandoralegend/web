cf8ed8a17d462a1cebca307dcb46dc61b2b6d570
pandoralegend <92421486+pandoralegend@users.noreply.github.com>
2021-10-18 12:27:41 +0800

    Delete static directory

--- static/js/13.b722fda5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpandora_legend=this.webpackJsonpandora_legend||[]).push([[13],{2128:function(n,a){},2153:function(n,a){}}]);
-//# sourceMappingURL=13.b722fda5.chunk.js.map
--- static/js/17.08d76919.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpandora_legend=this.webpackJsonpandora_legend||[]).push([[17],{2019:function(n,a){}}]);
-//# sourceMappingURL=17.08d76919.chunk.js.map
